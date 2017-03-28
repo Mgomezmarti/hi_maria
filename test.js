7e610fab0ebc11e62bce0c4103a03ba88a37693a
Mgomezmarti <maria.gomez@gmail.com>
2017-03-28 20:48:55 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+def buzz (n)
+  a = 1
+  for i in range(1,100):
+    if a % 3
+      if a % 5
+        print ('FizzBuzz')
+      else
+        print ('Fizz')
+    else
+      if a % 5
+        print ('Buzz')
+   
+        
+ 
